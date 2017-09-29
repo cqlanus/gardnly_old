8e2ec7161e8daca4f9335d8175c8fda8b8647abc
Chris Lanus <cqlanus@gmail.com>
2017-09-28 22:14:36 -0500

    fixed routing for plots and plantings

--- server/auth/filters.js
+++ server/auth/filters.js
@@ -1,6 +1,6 @@
 const mustBeLoggedIn = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send('You mustBeLoggedIn');
+    return res.status(401).send('You must be logged in!!!');
   }
   next();
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,8 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 router.use('/gardens', require('./gardens'));
-router.use('/plots', require('./users'));
-router.use('/plantings', require('./users'));
+router.use('/plots', require('./plots'));
+router.use('/plantings', require('./plantings'));
 router.use('/crops', require('./crops'));
 
 router.use((req, res, next) => {
