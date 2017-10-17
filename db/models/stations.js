7ed4c642ed94f8aa5f1f13adb0fec420ebc61f65
Chris Lanus - work <cqlanus@gmail.com>
2017-10-17 13:58:35 -0500

    actually updated model definition

--- db/models/stations.js
+++ db/models/stations.js
@@ -6,7 +6,15 @@ module.exports = function(sequelize, DataTypes) {
     station_name: DataTypes.STRING,
     state: DataTypes.STRING,
     center: DataTypes.GEOMETRY,
-    elevation: DataTypes.FLOAT
+    elevation: DataTypes.FLOAT,
+    last_frost_50: DataTypes.STRING,
+    last_frost_90: DataTypes.STRING,
+    first_frost_50: DataTypes.STRING,
+    first_frost_90: DataTypes.STRING,
+    season_length_50: DataTypes.STRING,
+    season_length_90: DataTypes.STRING,
+    ann_gdd_50: DataTypes.STRING,
+    ann_gdd_90: DataTypes.STRING,
   });
   return stations;
 };
