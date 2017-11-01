a4126a354cd50b9dc177938e2471dbd248fbfb26
Chris Lanus - work <cqlanus@gmail.com>
2017-11-01 12:11:14 -0500

    merge conflicts

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanWebPackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -11,7 +10,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].[chunkhash:8].bundle.js',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js',
     publicPath: '/',
   },
   module: {
@@ -38,24 +38,20 @@ module.exports = {
         use: [{
           loader: 'file-loader',
           options: {
-            name: '[name].[hash:8].[ext]'
+            name: '[name].[ext]'
           }
         }]
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('[name].[chunkhash:8].bundle.css'),
+    new ExtractTextPlugin('[name].bundle.css'),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor'],
       minChunks: 2
     }),
-    new CleanWebPackPlugin(path.resolve(__dirname, 'public'), {
-      watch: true
-    }),
     new HtmlWebpackPlugin({
       title: 'gardnly',
-      filename: 'index.html',
       template: 'app/index.html'
     })
   ],
